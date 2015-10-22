ad9538dc7ddcef59a12460838b0e86ca4d921956
Derek Gustafson <degustaf@gmail.com>
2015-10-22 20:34:45 +0000

    Added Suit to card class

--- ante/card.py
+++ ante/card.py
@@ -1,2 +1,18 @@
-Class Card:
-    pass
+"""
+Module to contain Card class that implements a standard playing card from a
+deck of 52 cards.
+"""
+
+class Card:
+    """
+    Class for cards.  Takes a 2 character string and converts it to a suit and
+    value.
+    """
+
+    Suits = {'C': 'Clubs', 'D': 'Diamonds', 'H': 'Hearts', 'S': 'Spades'}
+
+    def __init__(self, card_code):
+        """
+        Convert card_code into instance variables.
+        """
+        self.suit = self.Suits[card_code[1].upper()]
