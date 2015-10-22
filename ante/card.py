47d8e4ea4b76c7af32ad4e258aaffb69bc14f1d4
Derek Gustafson <degustaf@gmail.com>
2015-10-22 23:51:09 +0000

    Converted Card class to namedtuple

--- ante/card.py
+++ ante/card.py
@@ -2,17 +2,24 @@
 Module to contain Card class that implements a standard playing card from a
 deck of 52 cards.
 """
+import collections
 
-class Card:
+Card = collections.namedtuple('Card', ['value', 'suit'])
+SUITS = {'CDHS'}
+VALUES = ('ATJQK', range(2, 10))
+
+
+def create_card(card_code):
     """
-    Class for cards.  Takes a 2 character string and converts it to a suit and
-    value.
+    Create card instance based on value of card_code.
     """
+    if len(card_code) != 2:
+        raise ValueError("{} is an invalid card".format(card_code))
+    new_card = Card(list(card_code.upper()))
 
-    Suits = {'C': 'Clubs', 'D': 'Diamonds', 'H': 'Hearts', 'S': 'Spades'}
+    if new_card.value not in VALUES:
+        raise ValueError("{} is an invalid card value".format(new_card.value))
+    if new_card.suit not in SUITS:
+        raise ValueError("{} is an invalid suit".format(new_card.suit))
 
-    def __init__(self, card_code):
-        """
-        Convert card_code into instance variables.
-        """
-        self.suit = self.Suits[card_code[1].upper()]
+    return new_card
